07617de47201222dbc55b801995079346f1f84f8
oleast <oleast@stud.ntnu.no>
2019-02-24 16:39:56 +0100

    Remove initial CRA page, add SSP font, create basic page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const AppStyle = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  height: 100vh;
+`;
+
+export const App = () => {
+  return (
+    <AppStyle>
+      <Header />
+      <Main />
+      <Footer />
+    </AppStyle>
+  );
+};
 
 export default App;
