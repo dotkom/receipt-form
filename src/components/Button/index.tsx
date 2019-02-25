e9b01ae379d01913d8913848e9f652947f671f90
oleast <oleast@stud.ntnu.no>
2019-02-25 17:03:40 +0100

    Remove abstraction from Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,13 @@
 import React, { FC, HTMLProps } from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
+export const Button = styled.button`
   background: #0060a3;
   color: #fff;
-  box-shadow: 0 1px 3px 0 #333;
+  box-shadow: 0 1px 2px 0 #333;
   border-radius: 4px;
   padding: 0.8rem;
   font-size: 1rem;
   font-weight: 600;
   letter-spacing: 1px;
 `;
-
-export const Button: FC<HTMLProps<HTMLButtonElement>> = (props) => {
-  return (
-    <div>
-      <StyledButton>
-        { props.children }
-      </StyledButton>
-    </div>
-  );
-};
