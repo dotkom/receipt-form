94eda1df7e25e098c252ac7ed1b5e3b7eabe6221
oleast <oleast@stud.ntnu.no>
2019-03-02 10:19:55 +0100

    Add dropdown for committees

---
+++ src/components/Input/CommitteeDropdown.tsx
@@ -0,0 +1,49 @@
+import React, { ChangeEvent, useContext } from 'react';
+import styled from 'styled-components';
+
+import { ReceiptContext } from 'contexts/ReceiptData';
+import { ActionType } from 'hooks/useReceiptData';
+import { COMMITTEES } from 'models/comittees';
+
+import { Label } from './Base';
+import { Select } from './Dropdown';
+
+const Option = styled.option`
+
+`;
+
+const SelectContainer = styled.div`
+  margin: auto 0;
+`;
+
+export const CommitteeDropdown = () => {
+  const { state, dispatch } = useContext(ReceiptContext);
+
+  const onDropdownChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const key = event.target.value;
+    const committee = COMMITTEES.find((com) => com.group === key);
+    if (committee) {
+      dispatch({
+        type: ActionType.CHANGE,
+        data: { committee },
+      });
+    }
+  };
+
+  const selected = state.committee;
+  const defaultSelected = state.committee === null;
+
+  return (
+    <SelectContainer>
+      <Label>komité</Label>
+      <Select onChange={onDropdownChange}>
+        <Option disabled selected={defaultSelected}>Velg en komité</Option>
+        { COMMITTEES.map(({ group, name }) => (
+          <Option key={group} value={group} selected={selected ? selected.group === group : false}>
+            { name }
+          </Option>
+        )) }
+      </Select>
+    </SelectContainer>
+  );
+};
---
+++ src/components/Input/Dropdown.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+import styled from 'styled-components';
+
+import { BaseInputStyle } from './Base';
+
+export const Select = styled.select`
+  border: 2px solid #8e8e8e;
+  background: #ffffff;
+  ${BaseInputStyle}
+`;
