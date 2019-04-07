b677fab74b8126e93730ff9ecbb689e0f0a318da
oleast <oleast@stud.ntnu.no>
2019-04-07 15:25:46 +0200

    Add color prop to all icons

--- src/components/Icons/Edit.tsx
+++ src/components/Icons/Edit.tsx
@@ -2,11 +2,15 @@ import React, { HTMLProps } from 'react';
 
 import { colors } from 'constants/colors';
 
-export const Edit = ({ ref, ...props }: HTMLProps<SVGElement>) => {
+export interface IProps extends HTMLProps<SVGElement> {
+  color?: string;
+}
+
+export const Edit = ({ ref, color, ...props }: HTMLProps<SVGElement>) => {
   return (
     <svg width="100%" height="100%" viewBox="0 0 11 11" {...props}>
-      <path d="M7.14233 2L8.45555 3.10192L4.78248 7.47932L3.46926 6.3774L7.14233 2Z" fill={colors.darkGray} />
-      <path d="M3.10193 6.81519L4.41515 7.91711L2.65662 8.67937L3.10193 6.81519Z" fill={colors.darkGray} />
+      <path d="M7.14233 2L8.45555 3.10192L4.78248 7.47932L3.46926 6.3774L7.14233 2Z" fill={color || colors.darkGray} />
+      <path d="M3.10193 6.81519L4.41515 7.91711L2.65662 8.67937L3.10193 6.81519Z" fill={color || colors.darkGray} />
     </svg>
   );
 };
