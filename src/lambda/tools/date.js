c928dd1791da28d44c13b3f5b0ce4ccf5a2e9b06
oleast <oleast@stud.ntnu.no>
2019-04-10 12:42:53 +0200

    Remove compiled js

--- src/lambda/tools/date.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getCurrentDateString = function () {
-    var date = new Date();
-    return date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
-};
