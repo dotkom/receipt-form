c760babda5616788cd333e319e804dcea5f6999e
oleast <oleast@stud.ntnu.no>
2019-05-02 11:09:50 +0200

    Display response messages from backend in the submit Area

---
+++ src/components/Areas/Submit/ResponseMessage.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+import { colors } from 'constants/colors';
+import { IResultMessage } from 'lambda/handler';
+
+export interface IProps {
+  response: IResultMessage;
+}
+
+export interface IMessageProps {
+  statusCode: number;
+}
+
+const GOOD_STATUS_CODES = [200, 201];
+const BAD_STATUS_CODES = [400, 401, 403, 404, 500, 501, 502, 503, 504];
+
+const getStatusCodeColor = (code: number) => {
+  if (GOOD_STATUS_CODES.includes(code)) {
+    return colors.green;
+  } else if (BAD_STATUS_CODES.includes(code)) {
+    return colors.red;
+  } else {
+    return colors.orange;
+  }
+};
+
+const Message = styled.h3<IMessageProps>`
+  color: ${({ statusCode }) => getStatusCodeColor(statusCode)};
+`;
+
+const GENERIC_ERROR = `
+Noe ser ut til å ha gått galt under prosesseringen.
+`;
+
+export const ResponseMessage: FC<IProps> = ({ response }) => {
+  return <Message statusCode={response.statusCode}>{response.body ? response.body.message : GENERIC_ERROR}</Message>;
+};
