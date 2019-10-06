e0e859ebfbac7b83113ccb358428600b231e933d
oleast <oleast@stud.ntnu.no>
2019-10-06 20:24:39 +0200

    Add options for extra CC emails in env

--- src/constants/mail.ts
+++ src/constants/mail.ts
@@ -1,2 +1,3 @@
 export const SENDER_EMAIL = process.env.REACT_APP_SENDER_EMAIL || 'kvitteringsbot@online.ntnu.no';
 export const DESTINATION_EMAIL = process.env.REACT_APP_DESTINATION_EMAIL || 'kvitteringsbot@online.ntnu.no';
+export const EXTRA_CC_EMAILS = process.env.REACT_APP_EXTRA_CC_EMAILS || '';
