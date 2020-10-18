7bc96476c0bd699205d7de05c1aa64baf2469dc4
oleast <oleast@stud.ntnu.no>
2020-10-18 13:57:04 +0200

    Install next, remove react scripts

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const withImages = require('next-images');
+
+const nextConfig = {
+  webpack: (config, options) => {
+    config.plugins = config.plugins || [];
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins = [...config.resolve.plugins, new TsconfigPathsPlugin()];
+
+    return config;
+  },
+};
+
+module.exports = withImages(nextConfig);
