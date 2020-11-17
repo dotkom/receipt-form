61484486680d2db98f3bfc8772738f5fbb80716b
oleast <ole.stokker@bekk.no>
2020-11-17 23:13:49 +0100

    Install test libs and setup testing config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', 'src'],
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+};
---
+++ setupTests.js
@@ -0,0 +1,12 @@
+// optional: configure or set up a testing framework before each test
+// if you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// used for __tests__/testing-library.js
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
+
+import { setConfig } from 'next/config';
+import { publicRuntimeConfig, serverRuntimeConfig } from './next.config';
+
+// Make sure you can use "publicRuntimeConfig" and "serverRuntimeConfig" within tests.
+setConfig({ publicRuntimeConfig, serverRuntimeConfig });
