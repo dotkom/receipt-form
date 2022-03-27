5ba8e6100e6db5230ca618577d919620ecc6799a
Henrik Skog <henrikskog01@gmail.com>
2022-03-27 18:01:32 +0200

    fix(mail): read mail env from vercel

--- src/constants/mail.ts
+++ src/constants/mail.ts
@@ -1,3 +1,5 @@
 export const SENDER_EMAIL = process.env.NEXT_PRIVATE_SENDER_EMAIL || 'kvitteringsbot@online.ntnu.no';
 export const DESTINATION_EMAIL = process.env.NEXT_PRIVATE_DESTINATION_EMAIL || 'kvitteringsbot@online.ntnu.no';
+export const CLIENT_ID = process.env.CLIENT_ID || '';
+export const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 export const EXTRA_CC_EMAILS = process.env.NEXT_PRIVATE_EXTRA_CC_EMAILS || '';
