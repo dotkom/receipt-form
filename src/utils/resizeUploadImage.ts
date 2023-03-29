e0a7b3051f33b339be89bbba548025ec30408b25
jogt <jotjernshaugen@gmail.com>
2023-03-29 22:10:57 +0200

    Fixed linting

--- src/utils/resizeUploadImage.ts
+++ src/utils/resizeUploadImage.ts
@@ -1,55 +1,52 @@
 const MAX_SIZE = 1600;
 
 function rescaledResolution(width: number, height: number) {
-    const ratio = width / height;
+  const ratio = width / height;
 
-    if (width <= MAX_SIZE && height <= MAX_SIZE)
-        return { width, height };
+  if (width <= MAX_SIZE && height <= MAX_SIZE) return { width, height };
 
-    if (width > height) {
-        return {
-            width: MAX_SIZE,
-            height: Math.round(MAX_SIZE / ratio),
-        };
-    } else {
-        return {
-            width: Math.round(MAX_SIZE * ratio),
-            height: MAX_SIZE,
-        };
-    }
+  if (width > height) {
+    return {
+      width: MAX_SIZE,
+      height: Math.round(MAX_SIZE / ratio),
+    };
+  } else {
+    return {
+      width: Math.round(MAX_SIZE * ratio),
+      height: MAX_SIZE,
+    };
+  }
 }
 
 export function resizeUploadImage(file: File): Promise<File> {
-    const canvas = document.createElement("canvas");
-    const img = document.createElement("img");
+  const canvas = document.createElement('canvas');
+  const img = document.createElement('img');
 
-    return new Promise((resolve, reject) => {
-        img.onload = () => {
-            const { width, height } = rescaledResolution(img.width, img.height);
+  return new Promise((resolve, reject) => {
+    img.onload = () => {
+      const { width, height } = rescaledResolution(img.width, img.height);
 
-            canvas.width = width;
-            canvas.height = height;
+      canvas.width = width;
+      canvas.height = height;
 
-            const ctx = canvas.getContext("2d");
-            if (!ctx) {
-                reject(new Error("Could not get canvas context"));
-                return;
-            }
+      const ctx = canvas.getContext('2d');
+      if (!ctx) {
+        reject(new Error('Could not get canvas context'));
+        return;
+      }
 
-            ctx.drawImage(img, 0, 0, width, height);
+      ctx.drawImage(img, 0, 0, width, height);
 
-            canvas.toBlob(
-                (blob) => {
-                    if (!blob) {
-                        reject(new Error("Could not resize image"));
-                        return;
-                    }
+      canvas.toBlob((blob) => {
+        if (!blob) {
+          reject(new Error('Could not resize image'));
+          return;
+        }
 
-                    resolve(new File([blob], "kvittering.jpg", { type: "image/jpeg" }));
-                }
-            );
-        };
-        img.onerror = reject;
-        img.src = URL.createObjectURL(file);
-    });
+        resolve(new File([blob], 'kvittering.jpg', { type: 'image/jpeg' }));
+      });
+    };
+    img.onerror = reject;
+    img.src = URL.createObjectURL(file);
+  });
 }
